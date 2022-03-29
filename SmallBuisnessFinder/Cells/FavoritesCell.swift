b6fa3b72ba3f07f5a316229cf54146bcffe3c61f
Jacqueline DiMonte <jdimonte@umich.edu>
2022-03-28 21:00:56 -0400

    Favoriting works

--- SmallBuisnessFinder/Cells/FavoritesCell.swift
+++ SmallBuisnessFinder/Cells/FavoritesCell.swift
@@ -28,7 +28,21 @@ class FavoritesCell : UITableViewCell {
         self.category.font = .systemFont(ofSize: 12)
         self.location.font = .systemFont(ofSize: 12)
         
-        self.starButton.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+        var blah = false
+        let usersRef = Database.database().reference(withPath:  "users")
+        let array_ref = usersRef.child((Auth.auth().currentUser?.email?.safeDatabaseKey())!).child("likes")
+        let queryRef = array_ref.queryOrdered(byChild: "name").queryEqual(toValue: self.name.text)
+        queryRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            for snap in snapshot.children {
+                blah = true
+            }
+        })
+        if blah {
+            self.starButton.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
+        }
+        else{
+            self.starButton.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
+        }
         
         self.img.frame = CGRect(x: 5, y: 10, width: 60, height: 60)
         self.starButton.frame = CGRect(x: UIScreen.main.bounds.width - 65, y: 25, width: 30, height: 30)
@@ -52,20 +66,27 @@ class FavoritesCell : UITableViewCell {
         if self.starButton.currentBackgroundImage == UIImage(systemName: "star") {
             self.starButton.setBackgroundImage(UIImage(systemName: "star.fill"), for: .normal)
             //like business!!
-            //1) connect to user in database
+            //1) connect to user's likes in database
             let ref = Database.database().reference(withPath:  "users")
             let array_ref = ref.child((Auth.auth().currentUser?.email?.safeDatabaseKey())!)
+            let add_to_ref = array_ref.child("likes").childByAutoId()
+            add_to_ref.setValue(["name": self.name.text]) //like business
             //2)add to array
         } else if self.starButton.currentBackgroundImage == UIImage(systemName: "star.fill") {
             self.starButton.setBackgroundImage(UIImage(systemName: "star"), for: .normal)
             //unlike business!!
-            //1) connect to user in database
-            let ref = Database.database().reference(withPath:  "users")
-            let array_ref = ref.child((Auth.auth().currentUser?.email?.safeDatabaseKey())!)
-            let thisPostRef = array_ref.childByAutoId //create a new post node
-            thisPostRef().setValue("here's a new post") //store the post in it
-            //let array_ref = ref.child((Auth.auth().currentUser?.email)!).child("likes")
+            //1) connect to user's likes in database
+            let usersRef = Database.database().reference(withPath:  "users")
+            let array_ref = usersRef.child((Auth.auth().currentUser?.email?.safeDatabaseKey())!).child("likes")
             //2)remove from array
+            let queryRef = array_ref.queryOrdered(byChild: "name").queryEqual(toValue: self.name.text)
+            queryRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                for snap in snapshot.children {
+                    let userSnap = snap as! DataSnapshot
+                    let uid = userSnap.key
+                    array_ref.child(uid).removeValue()
+                }
+            })
         }
     }
     
